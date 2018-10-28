c83fca6a19a6158c4413a49d563d81d0363828f9
ximing chen <38732371+ximingITC@users.noreply.github.com>
2018-10-27 22:45:18 -0400

    test

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -28,12 +28,14 @@ void KalmanFilter::Predict() {
 	P_ = F_ * P_ * F_.transpose() + Q_; // update to get P(k+1|k)
 }
 
+/* my code =====================================================
 void KalmanFilter::Update(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
-  */
+  
 	// here P is represented by P(k|k)
+	
 	VectorXd y = z - H_ * x_;
 	MatrixXd Ht = H_.transpose();
 	MatrixXd S = H_ * P_ * Ht + R_;
@@ -46,11 +48,12 @@ void KalmanFilter::Update(const VectorXd &z) {
 	P_ = (I - K * H_) * P_;
 }
 
+
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
-  */
+  
   // the same procedure as above but replacing the H function with Hj
 	// first compute the predicted x in z coordinate
 	float px = x_(0);
@@ -85,5 +88,61 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 	int dim = x_.size();
 	MatrixXd I = MatrixXd::Identity(dim, dim);
 	x_ = x_ + (K * y);
+	P_ = (I - K * H_) * P_;
+}
+*/
+
+
+void KalmanFilter::Update(const VectorXd &z) {
+	// update the state by using Kalman Filter equations
+
+	// measurement update
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd PHt = P_ * H_.transpose();
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd K = PHt * S.inverse();
+
+	//new state
+	x_ = x_ + (K * y);
+	int x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+	// update the state by using Extended Kalman Filter equations
+
+	// get components of predicted state
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+
+	// get components of radar measurement space
+	float rho = sqrt(px * px + py * py);
+	float phi = atan2(py, px);
+	float rho_dot = (rho != 0 ? (px * vx + py * vy) / rho : 0);
+
+	// define predicted position and speed
+	VectorXd z_pred(3);
+	z_pred << rho, phi, rho_dot;
+
+	// measurement update
+	VectorXd y = z - z_pred;
+
+	// normalize angle
+	double width = 2 * M_PI;   //
+	double offsetValue = y(1) + M_PI;   // value relative to 0
+	y(1) = (offsetValue - (floor(offsetValue / width) * width)) - M_PI;
+
+	MatrixXd PHt = P_ * H_.transpose();
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd K = PHt * S.inverse();
+
+	// new state
+	x_ = x_ + (K * y);
+	int x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
 	P_ = (I - K * H_) * P_;
 }
